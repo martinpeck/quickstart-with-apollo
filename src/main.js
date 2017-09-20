8d7bb6a3d343d3a68c40980e16cc8dd532294a03
Martin Peck <martinjohnpeck@gmail.com>
2017-09-20 19:30:02 +0100

    init commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import VueApollo from 'vue-apollo'
+
+// Paste your endpoint for the Simple API here.
+// Info: https://github.com/graphcool-examples/vue-apollo-instagram-example#2-create-graphql-api-with-graphcool
+const networkInterface = createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cj7t4epwa0nxk0181qyc1v37c'});
+
+const apolloClient = new ApolloClient({
+  networkInterface,
+})
+
+// Install the vue plugin
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
+// Start the app
+new Vue({
+  el: '#app',
+  apolloProvider,
+  render: h => h(App)
+})
